5ce67c5ceb00e87d510750df20dbc13d228008cc
Anthony <86688211+Origami2553@users.noreply.github.com>
2023-06-14 19:57:37 -0400

    1.1.1

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,18 @@
 // Add class to elements when they enter the viewport
 function fadeElementsIn() {
-  const elements = document.querySelectorAll('.fade-in');
-
+    const elements = document.querySelectorAll('.fade-in');
+  
+    elements.forEach((element) => {
+      const position = element.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+  
+      if (position < windowHeight * 0.8) {
+        element.classList.add('active');
+      }
+    });
+  }
+  
+  // Listen for scroll events and fade in elements
+  window.addEventListener('scroll', fadeElementsIn);
+  window.addEventListener('load', fadeElementsIn);
+  
