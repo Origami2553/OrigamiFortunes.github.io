2bc608a79e80aa5f3e7fe4c6ecf48cbfb07167b9
Anthony <86688211+Origami2553@users.noreply.github.com>
2023-06-14 20:12:02 -0400

    update

--- demo/script.js
+++ demo/script.js
@@ -15,4 +15,26 @@ function fadeElementsIn() {
   // Listen for scroll events and fade in elements
   window.addEventListener('scroll', fadeElementsIn);
   window.addEventListener('load', fadeElementsIn);
+
+  // Slideshow functionality
+const slider = document.querySelector('.slider');
+const images = slider.querySelectorAll('img');
+
+let currentImageIndex = 0;
+let nextImageIndex = 1;
+
+// Show the next image in the slideshow
+function showNextImage() {
+  images[currentImageIndex].style.opacity = 0;
+  images[nextImageIndex].style.opacity = 1;
+
+  currentImageIndex = nextImageIndex;
+  nextImageIndex = (nextImageIndex + 1) % images.length;
+
+  setTimeout(showNextImage, 3000); // Change image every 3 seconds (adjust as desired)
+}
+
+// Start the slideshow
+showNextImage();
+
   
